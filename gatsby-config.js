5e28e095f71f6bc8f468105e6e3afa15f0f1d111
Maciej Myśliński <maciej.myslinski@netguru.co>
2017-10-13 11:28:18 +0200

    Use gatsby styled-components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     keywords: '',
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-google-analytics`,
